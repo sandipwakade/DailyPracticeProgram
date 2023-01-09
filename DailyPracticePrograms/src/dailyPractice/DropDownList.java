5c741d23ea2734ec5c98a3750eb5d0b4953e235a
Lenovo <Lenovo@DESKTOP-AUGHFJE>
2023-01-09 16:02:56 +0530

    add wakade

--- DailyPracticePrograms/src/dailyPractice/DropDownList.java
+++ DailyPracticePrograms/src/dailyPractice/DropDownList.java
@@ -9,6 +9,7 @@ public class DropDownList {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\velocityclassnotes\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		System.out.println("sandip");
+		System.out.println("Wakade");
 	}
 
 }
