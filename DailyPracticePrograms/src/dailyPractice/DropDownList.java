26b7e6a9b0fd21b66c6cf88630bac114accd1afd
Lenovo <Lenovo@DESKTOP-AUGHFJE>
2023-01-09 15:55:51 +0530

    created new test case for daily practice program(user story 107)

---
+++ DailyPracticePrograms/src/dailyPractice/DropDownList.java
@@ -0,0 +1,14 @@
+package dailyPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DropDownList {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\velocityclassnotes\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		System.out.println("sandip");
+	}
+
+}
