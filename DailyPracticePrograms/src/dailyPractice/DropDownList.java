e97fd84aa7d79aecb0c358b67718e3f403d457dc
Lenovo <Lenovo@DESKTOP-AUGHFJE>
2023-01-09 16:09:52 +0530

    commenet sandip and wakade

--- DailyPracticePrograms/src/dailyPractice/DropDownList.java
+++ DailyPracticePrograms/src/dailyPractice/DropDownList.java
@@ -8,8 +8,8 @@ public class DropDownList {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\velocityclassnotes\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-		System.out.println("sandip");
-		System.out.println("Wakade");
+		//System.out.println("sandip");
+		//System.out.println("Wakade");
 		
 	}
 
